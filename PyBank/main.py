d3d6790c6b5f12245ad839626886ecb288ca54ca
Alec Banerji <alec.banerji@gmail.com>
2023-07-03 21:02:37 -0700

    Added comments. Cleaned out output format.

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,30 +1,3 @@
-""" PyBank Instructions
-In this Challenge, you are tasked with creating a Python script to analyze the financial records of your company. You will be given a financial dataset called budget_data.csv. The dataset is composed of two columns: "Date" and "Profit/Losses".
-
-Your task is to create a Python script that analyzes the records to calculate each of the following values:
-
-The total number of months included in the dataset
-
-The net total amount of "Profit/Losses" over the entire period
-
-The changes in "Profit/Losses" over the entire period, and then the average of those changes
-
-The greatest increase in profits (date and amount) over the entire period
-
-The greatest decrease in profits (date and amount) over the entire period
-
-Your analysis should align with the following results:
-
-Financial Analysis
-----------------------------
-Total Months: 86
-Total: $22564198
-Average Change: $-8311.11
-Greatest Increase in Profits: Aug-16 ($1862002)
-Greatest Decrease in Profits: Feb-14 ($-1825558)
-In addition, your final script should both print the analysis to the terminal and export a text file with the results. """
-
-
 ### From Day 2, Activity 8
 
 # First we'll import the os module
@@ -36,10 +9,9 @@ import csv
 
 csvpath = os.path.join('Resources', 'budget_data.csv')
 
-# Counters
+# Counters at extremes
 total_months = 0
 net_profit_losses = 0
-
 
 sum_profit_losses = 0
 
@@ -50,7 +22,6 @@ greatest_decrease = 999999999999
 greatest_decrease_month = ""
 
 # Method 2: Improved Reading using CSV module
-
 with open(csvpath) as csvfile:
 
     # CSV reader specifies delimiter and variable that holds contents
@@ -60,33 +31,57 @@ with open(csvpath) as csvfile:
     csv_header = next(csvreader)
 
     # Read each row of data after the header
-    # This is the main loop!
+
+    ### This is the main loop! ###
 
     for row in csvreader:
+
+        # Define variables by column. 
         date = row[0]
+
+        # Convert profit_loss to integer!
         profit_loss = int(row[1])
 
+        # Add 1 to total months counter. 
         total_months += 1
+
+        # Add current row profit_loss to net_profit_loss.
         net_profit_losses = net_profit_losses + profit_loss
 
+        # Check to see if a change occurred. (all but first row of table)
         if total_months > 1:
+
+            # Caculate change using previous row value compared to current row.
             change = profit_loss - last_profit_losses
 
+            # Add change to profit_loss sum.
             sum_profit_losses = sum_profit_losses + change
             
-
+            # Check if the last change is greater than the most recent greatest stored increase.
             if change > greatest_increase:
+
+                # Reset the greatest stored increase to the new greatest value. Store the month.
                 greatest_increase = change
                 greatest_increase_month = row[0]
 
+            # Check if the last change is less than the most recent greatest stored decrease.
             if change < greatest_decrease:
+
+                # Reset the greatest stored decrease to the new greatest value. Store the month.
                 greatest_decrease = change
                 greatest_decrease_month = row[0]
 
+        # Store the profit loss in another variable. 
+        # The next time through the loop, the previous value (now stored) can be used as comparison to calculate change.
         last_profit_losses = int(row[1])
-        
+
+    # Finally, outside the main loop, calculate ave_change.
+    # since you have no change for the first row, subtract 1 from total months.    
     ave_change = sum_profit_losses / (total_months - 1)
     
+    # Print summary info to screen
+    print("Financial Analysis")
+    print("----------------------------")
     print(f"Total Months: {total_months}")
     print(f'Total: ${net_profit_losses}')
     print(f"Average Change: ${round(ave_change,2)}")
