ff1481fa4f1e8a1616de0400a8eeae9d4fd12329
Alec Banerji <alec.banerji@gmail.com>
2023-07-03 21:43:20 -0700

    PyPoll started. Total Votes successfully calculated.

---
+++ PyPoll/main.py
@@ -0,0 +1,32 @@
+import os
+import csv
+
+csvpath = os.path.join('Resources', 'election_data.csv')
+
+total_votes = 0
+candidates = []
+total_charles = 0
+total_diana = 0
+total_raymon = 0
+
+# Method 2: Improved Reading using CSV module
+with open(csvpath) as csvfile:
+
+    # CSV reader specifies delimiter and variable that holds contents
+    csvreader = csv.reader(csvfile, delimiter=',')
+
+    # Read the header row first (skip this step if there is no header)
+    csv_header = next(csvreader)
+
+    # Read each row of data after the header
+
+    ### This is the main loop! ###
+
+    for row in csvreader:
+
+        total_votes += 1
+        ballot_id = row[0]
+        county = row[1]
+        candidate = row[2]
+
+    print(f"Total Votes: {total_votes}")
